0045f8031682b981bd759995367b5623ba02bcf6
Zaman <zaman@sgcsoft.net>
2023-01-31 15:39:26 +0600

    enable cors

--- employee_portal_api/app/settings.py
+++ employee_portal_api/app/settings.py
@@ -62,6 +62,10 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ORIGIN_ALLOW_ALL = True
+
 ROOT_URLCONF = 'app.urls'
 
 TEMPLATES = [
